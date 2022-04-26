3c8317cfb5f0d409b5234d7447fa1172fcedc4d3
emleshi <emilyzhoushi@gmail.com>
2022-04-26 15:48:29 -0500

    Debugging problem

--- Work/sears.py
+++ Work/sears.py
@@ -1,15 +1,15 @@
-# One morning, you go out and place a dollar bill 
-# on the sidewalk by the Sears tower in Chicago. 
-# Each day thereafter, you go out double the number 
-# of bills. How long does it take for the stack 
-# of bills to exceed the height of the tower?
+# sears.py
 
-h_tower = 442                   # height of Sear's tower in m
-h_dollar = 0.11 * (10 ** -3)    # height of dollar bill in m
-num_bills = 1
-day = 1
+bill_thickness = 0.11 * 0.001    # Meters (0.11 mm)
+sears_height   = 442             # Height (meters)
+num_bills      = 1
+day            = 1
 
-while ((h_dollar * num_bills) < h_tower):
+while num_bills * bill_thickness < sears_height:
+    print(day, num_bills, num_bills * bill_thickness)
+    day = days + 1
     num_bills = num_bills * 2
-    day += 1
-print (day)
+
+print('Number of days', day)
+print('Number of bills', num_bills)
+print('Final height', num_bills * bill_thickness)
