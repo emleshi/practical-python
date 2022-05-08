4f3bd77127894eafe5bdaac6a137db0b1203087d
Emily Shi <emilyzhoushi@gmail.com>
2022-05-07 20:38:38 -0500

    Uses f-strings to create output

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -25,8 +25,8 @@ while principal > 0:
     if principal < 0:
         principal = 0
     
-    print(month, round(total_paid,2), round(principal,2))
+    print(f'{month} {round(total_paid,2)} {round(principal,2)}')
 
 
-print('Total paid', round(total_paid,2))
-print('Months', month)
+print(f'Total paid {round(total_paid,2)}')
+print(f'Months {month}')
