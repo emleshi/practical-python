c5b2937a701c27c546d7aee98f208ba4292b9595
Emily Shi <emilyzhoushi@gmail.com>
2022-05-07 21:02:01 -0500

    Init pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,18 @@
 # pcost.py
 #
 # Exercise 1.27
+
+# Total cost variable
+total = 0
+
+# Open portfolio file
+with open('Data/portfolio.csv', 'rt') as file:
+    # Get header
+    header = next(file)
+
+    # Add cost of purchasing all shares
+    for line in file:
+        stock = line.split(',')
+        total += int(stock[1]) * float(stock[2])
+
+print(f'Total cost {total}')
